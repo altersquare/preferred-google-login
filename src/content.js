8fd8e9c38939f04981492dad9257f66c5e71221b
Rohan Dhamapurkar <dhamapurkar54@gmail.com>
2025-03-05 15:30:32 +0530

    fix: indentation

--- src/content.js
+++ src/content.js
@@ -1,33 +1,35 @@
 // Immediately invoked function expression (IIFE) to execute code on page load.
 (async () => {
-    try {
-        // Retrieve stored settings from Chrome storage.
-        const { isEnabled } = await getFromStorage("isEnabled"); // Get whether the extension is enabled.
-        const { domainEmails } = await getFromStorage("domainEmails"); // Get the domain-email pairs.
+	try {
+		// Retrieve stored settings from Chrome storage.
+		const { isEnabled } = await getFromStorage("isEnabled"); // Get whether the extension is enabled.
+		const { domainEmails } = await getFromStorage("domainEmails"); // Get the domain-email pairs.
 
-        // If on a Chrome internal page (e.g., extensions page), exit.
-        if (window.location.protocol === "chrome:") return;
+		// If on a Chrome internal page (e.g., extensions page), exit.
+		if (window.location.protocol === "chrome:") return;
 
-        // If the extension is explicitly disabled, exit.
-        if (isEnabled !== undefined && !isEnabled) {
-            console.log("Extension is disabled.");
-            return;
-        }
+		// If the extension is explicitly disabled, exit.
+		if (isEnabled !== undefined && !isEnabled) {
+			console.log("Extension is disabled.");
+			return;
+		}
 
-        // Check if the current URL's hostname is in the allowed domains list.
-        const emailForDomain = Object.keys(domainEmails).find(domain => window.location.hostname.includes(domain));
+		// Check if the current URL's hostname is in the allowed domains list.
+		const emailForDomain = Object.keys(domainEmails).find((domain) =>
+			window.location.hostname.includes(domain)
+		);
 
-        // If the domain is not allowed or no email is set for it, exit.
-        if (!emailForDomain) {
-            console.log("Domain not allowed or no email set for this domain.");
-            return;
-        }
+		// If the domain is not allowed or no email is set for it, exit.
+		if (!emailForDomain) {
+			console.log("Domain not allowed or no email set for this domain.");
+			return;
+		}
 
-        // Add the authuser parameter to the URL.
-        setAuthUser(domainEmails[emailForDomain]);
-    } catch (error) {
-        console.error("Error:", error); // Log any errors.
-    }
+		// Add the authuser parameter to the URL.
+		setAuthUser(domainEmails[emailForDomain]);
+	} catch (error) {
+		console.error("Error:", error); // Log any errors.
+	}
 })();
 
 /**
@@ -36,7 +38,6 @@
  * @param {string} authUserEmail The email address to use for the authuser parameter.
  */
 async function setAuthUser(authUserEmail) {
-
 	const data = await getFromStorage(window.location.hostname); // Check if already set for this domain
 
 	if (data[window.location.hostname]) {
@@ -47,22 +48,21 @@ async function setAuthUser(authUserEmail) {
 
 	await setStorage(window.location.hostname, true); // Set a flag in storage indicating that we have modified this domain
 
-    const currentURL = window.location.href;
-    const authuserParam = "authuser=" + authUserEmail;
+	const currentURL = window.location.href;
+	const authuserParam = "authuser=" + authUserEmail;
 
-    // Check if the authuser parameter is already present in the URL.
-    if (currentURL.includes(authuserParam)) {
-        console.log("Authuser parameter already present.");
-        return;
-    }
+	// Check if the authuser parameter is already present in the URL.
+	if (currentURL.includes(authuserParam)) {
+		console.log("Authuser parameter already present.");
+		return;
+	}
 
-    // Add the authuser parameter to the URL.
-    let newURL = currentURL.includes("?")
-        ? currentURL + "&" + authuserParam // Add with & if query params are present
-        : currentURL.replace(/(\.com).*/, "$1") + "?" + authuserParam; // Add with ? if no query params are present
+	// Add the authuser parameter to the URL.
+	let newURL = currentURL.includes("?")
+		? currentURL + "&" + authuserParam // Add with & if query params are present
+		: currentURL.replace(/(\.com).*/, "$1") + "?" + authuserParam; // Add with ? if no query params are present
 
-
-    window.location.href = newURL; // Redirect to the new URL.
+	window.location.href = newURL; // Redirect to the new URL.
 }
 
 /**
@@ -72,8 +72,8 @@ async function setAuthUser(authUserEmail) {
  * @throws {Error} If the key is not a string.
  */
 function validateAndMutateKey(key) {
-    if (typeof key !== "string") throw new Error("Invalid key");
-    return key;
+	if (typeof key !== "string") throw new Error("Invalid key");
+	return key;
 }
 
 /**
@@ -82,8 +82,8 @@ function validateAndMutateKey(key) {
  * @returns {Promise<any>} A promise that resolves with the retrieved data.
  */
 async function getFromStorage(key) {
-    let storageKey = validateAndMutateKey(key); // Validate the key.
-    return await chrome.storage.local.get([storageKey]);
+	let storageKey = validateAndMutateKey(key); // Validate the key.
+	return await chrome.storage.local.get([storageKey]);
 }
 
 /**
@@ -92,8 +92,8 @@ async function getFromStorage(key) {
  * @returns {Promise<void>} A promise that resolves when the data is removed.
  */
 async function removeFromStorage(key) {
-    let storageKey = validateAndMutateKey(key); // Validate the key.
-    return await chrome.storage.local.remove(storageKey);
+	let storageKey = validateAndMutateKey(key); // Validate the key.
+	return await chrome.storage.local.remove(storageKey);
 }
 
 /**
@@ -103,6 +103,6 @@ async function removeFromStorage(key) {
  * @returns {Promise<void>} A promise that resolves when the data is set.
  */
 async function setStorage(key, value) {
-    let storageKey = validateAndMutateKey(key); // Validate the key.
-    return await chrome.storage.local.set({ [storageKey]: value });
+	let storageKey = validateAndMutateKey(key); // Validate the key.
+	return await chrome.storage.local.set({ [storageKey]: value });
 }
--- src/popup.js
+++ src/popup.js
@@ -1,167 +1,164 @@
 // Global array of Google domains
 const googleDomains = {
-   "google": "google.com",
-    "maps": "maps.google.com",
-    "news": "news.google.com",
-    "translate": "translate.google.com",
-    "drive": "drive.google.com",
-    "docs": "docs.google.com",
-    "mail": "mail.google.com",
-    "photos": "photos.google.com",
-    "keep": "keep.google.com",
-    "meet": "meet.google.com",
-    "classroom": "classroom.google.com",
-    "play": "play.google.com",
-    "developers": "developers.google.com",
-    "cloud": "cloud.google.com",
-    "adsense": "adsense.google.com",
-    "business": "business.google.com",
-    "merchants": "merchants.google.com",
-    "tagmanager": "tagmanager.google.com",
-    "marketingplatform": "marketingplatform.google.com",
-    "youtube": "youtube.com",
-    "studio": "studio.youtube.com",
-    "artsandculture": "artsandculture.google.com",
-    "store": "store.google.com",
-    "wearos": "wearos.google.com",
-    "domains": "domains.google.com",
-    "fi": "fi.google.com",
-    "one": "one.google.com",
-    "pay": "pay.google.com",
-    "shopping": "shopping.google.com",
-    "fonts": "fonts.google.com",
-    "labs": "labs.google.com",
-    "ai": "ai.google",
+	google: "google.com",
+	maps: "maps.google.com",
+	news: "news.google.com",
+	translate: "translate.google.com",
+	drive: "drive.google.com",
+	docs: "docs.google.com",
+	mail: "mail.google.com",
+	photos: "photos.google.com",
+	keep: "keep.google.com",
+	meet: "meet.google.com",
+	classroom: "classroom.google.com",
+	play: "play.google.com",
+	developers: "developers.google.com",
+	cloud: "cloud.google.com",
+	adsense: "adsense.google.com",
+	business: "business.google.com",
+	merchants: "merchants.google.com",
+	tagmanager: "tagmanager.google.com",
+	marketingplatform: "marketingplatform.google.com",
+	youtube: "youtube.com",
+	studio: "studio.youtube.com",
+	artsandculture: "artsandculture.google.com",
+	store: "store.google.com",
+	wearos: "wearos.google.com",
+	domains: "domains.google.com",
+	fi: "fi.google.com",
+	one: "one.google.com",
+	pay: "pay.google.com",
+	shopping: "shopping.google.com",
+	fonts: "fonts.google.com",
+	labs: "labs.google.com",
+	ai: "ai.google",
 };
 
 document.addEventListener("DOMContentLoaded", handleDOMLoad);
 
 async function handleDOMLoad() {
-    const toggle = document.getElementById("authuserToggle");
-    const domainEmailList = document.getElementById("domainEmailList");
-    const addButton = document.getElementById("addButton");
-    const saveButton = document.getElementById("saveButton");
-    saveButton.disabled = true;
+	const toggle = document.getElementById("authuserToggle");
+	const domainEmailList = document.getElementById("domainEmailList");
+	const addButton = document.getElementById("addButton");
+	const saveButton = document.getElementById("saveButton");
+	saveButton.disabled = true;
 
-    // Load the toggle state from storage
-    let { isEnabled } = await getFromStorage("isEnabled");
-    toggle.checked = isEnabled === undefined ? true : isEnabled;
+	// Load the toggle state from storage
+	let { isEnabled } = await getFromStorage("isEnabled");
+	toggle.checked = isEnabled === undefined ? true : isEnabled;
 
-    // Save the toggle state when it changes
-    toggle.addEventListener("change", async () => {
-        await setStorage("isEnabled", toggle.checked);
-        enableSaveButton();
-    });
+	// Save the toggle state when it changes
+	toggle.addEventListener("change", async () => {
+		await setStorage("isEnabled", toggle.checked);
+		enableSaveButton();
+	});
 
-    // Add a new domain-email pair
-    addButton.addEventListener("click", () => {
-        addDomainEmailPair(domainEmailList);
-        domainEmailList.scrollTop = domainEmailList.scrollHeight;
-        enableSaveButton();
-    });
+	// Add a new domain-email pair
+	addButton.addEventListener("click", () => {
+		addDomainEmailPair(domainEmailList);
+		domainEmailList.scrollTop = domainEmailList.scrollHeight;
+		enableSaveButton();
+	});
 
-    // Save changes
-    saveButton.addEventListener("click", handleSaveClick);
+	// Save changes
+	saveButton.addEventListener("click", handleSaveClick);
 
-    // Load domainEmails from storage
-    let { domainEmails } = await getFromStorage("domainEmails");
+	// Load domainEmails from storage
+	let { domainEmails } = await getFromStorage("domainEmails");
 
-    // Set default domainEmails if none exist
-    if (domainEmails === undefined) {
-        domainEmails = {
-            "youtube.com": "user@gmail.com",
-        };
-        await setStorage("domainEmails", domainEmails);
+	// Set default domainEmails if none exist
+	if (domainEmails === undefined) {
+		domainEmails = {
+			"youtube.com": "user@gmail.com",
+		};
+		await setStorage("domainEmails", domainEmails);
 	}
 
-    // Populate the domain-email list
-    populateDomainEmailList(domainEmailList, domainEmails, saveButton);
+	// Populate the domain-email list
+	populateDomainEmailList(domainEmailList, domainEmails, saveButton);
 }
 
 function populateDomainEmailList(container, domainEmails, saveButton) {
-    container.innerHTML = "";
+	container.innerHTML = "";
 
-    for (const [domain, email] of Object.entries(domainEmails)) {
-        addDomainEmailPair(container, domain, email, saveButton);
-    }
+	for (const [domain, email] of Object.entries(domainEmails)) {
+		addDomainEmailPair(container, domain, email, saveButton);
+	}
 }
 
-function addDomainEmailPair(container, domain = "", email = "",saveButton) {
-    
+function addDomainEmailPair(container, domain = "", email = "", saveButton) {
 	const domainEmailContainer = document.createElement("div");
-    domainEmailContainer.className = "domain-email-container";
-    
+	domainEmailContainer.className = "domain-email-container";
+
 	const listContainer = document.createElement("div");
 	listContainer.className = "input-domain-container"; // Add class for styling
 
-    // Domain input with datalist for autocomplete
-    const domainInput = document.createElement("input");
+	// Domain input with datalist for autocomplete
+	const domainInput = document.createElement("input");
 	domainInput.className = "domain-input";
-    domainInput.type = "text";
-    domainInput.placeholder = "Search or select domain...";
-    domainInput.value = domain;
-    domainInput.setAttribute("list", `domain-list-${Math.random()}`);
-    listContainer.appendChild(domainInput);	
+	domainInput.type = "text";
+	domainInput.placeholder = "Search or select domain...";
+	domainInput.value = domain;
+	domainInput.setAttribute("list", `domain-list-${Math.random()}`);
+	listContainer.appendChild(domainInput);
 
-    // Datalist for autocomplete suggestions
-    const domainDataList = document.createElement("datalist");
-    domainDataList.id = domainInput.getAttribute("list");
-    Object.keys(googleDomains).forEach(googleDomain => {
-        const option = document.createElement("option");
-        option.value = googleDomain;
-        domainDataList.appendChild(option);
-    });
-    listContainer.appendChild(domainDataList);
+	// Datalist for autocomplete suggestions
+	const domainDataList = document.createElement("datalist");
+	domainDataList.id = domainInput.getAttribute("list");
+	Object.keys(googleDomains).forEach((googleDomain) => {
+		const option = document.createElement("option");
+		option.value = googleDomain;
+		domainDataList.appendChild(option);
+	});
+	listContainer.appendChild(domainDataList);
 
 	const domainErrorMessage = document.createElement("div");
-    domainErrorMessage.className = "error-message";
-    domainErrorMessage.textContent = "";
-    listContainer.appendChild(domainErrorMessage);
+	domainErrorMessage.className = "error-message";
+	domainErrorMessage.textContent = "";
+	listContainer.appendChild(domainErrorMessage);
 
 	domainEmailContainer.appendChild(listContainer);
 
 	const emailContainer = document.createElement("div");
 	emailContainer.className = "input-email-container"; // Add class for styling
-    // Email input
+	// Email input
 
-    const emailInput = document.createElement("input");
-    emailInput.type = "text";
-    emailInput.placeholder = "Enter email...";
-    emailInput.value = email;
-    emailContainer.appendChild(emailInput);
+	const emailInput = document.createElement("input");
+	emailInput.type = "text";
+	emailInput.placeholder = "Enter email...";
+	emailInput.value = email;
+	emailContainer.appendChild(emailInput);
 
-
-    const emailErrorMessage = document.createElement("div");
-    emailErrorMessage.className = "error-message";
-    emailErrorMessage.textContent = "";
-    emailContainer.appendChild(emailErrorMessage);
+	const emailErrorMessage = document.createElement("div");
+	emailErrorMessage.className = "error-message";
+	emailErrorMessage.textContent = "";
+	emailContainer.appendChild(emailErrorMessage);
 
 	domainEmailContainer.appendChild(emailContainer);
 
+	// Remove button with cross icon
+	const removeButton = document.createElement("div");
+	removeButton.className = "remove-button";
+	removeButton.innerHTML = "&times;";
+	removeButton.addEventListener("click", async () => {
+		const domain = domainInput.value.trim();
+		const { domainEmails } = await getFromStorage("domainEmails");
+		delete domainEmails[domain];
+		await setStorage("domainEmails", domainEmails);
+		container.removeChild(domainEmailContainer);
+		enableSaveButton();
+	});
+	domainEmailContainer.appendChild(removeButton);
 
-    // Remove button with cross icon
-    const removeButton = document.createElement("div");
-    removeButton.className = "remove-button";
-    removeButton.innerHTML = "&times;"; 
-    removeButton.addEventListener("click", async () => {
-        const domain = domainInput.value.trim();
-        const { domainEmails } = await getFromStorage("domainEmails");
-        delete domainEmails[domain];
-        await setStorage("domainEmails", domainEmails);
-        container.removeChild(domainEmailContainer);
-        enableSaveButton();
-    });
-    domainEmailContainer.appendChild(removeButton);
+	// Add the row to the container
+	container.appendChild(domainEmailContainer);
 
-    // Add the row to the container
-    container.appendChild(domainEmailContainer);
-
-    // Validation for email input
+	// Validation for email input
 	emailInput.addEventListener("input", () => {
 		enableSaveButton();
-	
+
 		const emailValue = emailInput.value.trim();
-	
+
 		if (emailValue === "") {
 			emailErrorMessage.textContent = "Email is required";
 			emailErrorMessage.style.display = "block";
@@ -173,15 +170,17 @@ function addDomainEmailPair(container, domain = "", email = "",saveButton) {
 		}
 	});
 
-    // Validation for duplicate domains
-    domainInput.addEventListener("input",  () => {
+	// Validation for duplicate domains
+	domainInput.addEventListener("input", () => {
 		enableSaveButton();
 		const trimmedDomain = domainInput.value.trim();
 
-		const domains = Array.from(container.querySelectorAll(".domain-input"))
-			.map(input => input.value.trim());
+		const domains = Array.from(
+			container.querySelectorAll(".domain-input")
+		).map((input) => input.value.trim());
 
-		const isDuplicate = domains.filter(d => d === trimmedDomain).length > 1;
+		const isDuplicate =
+			domains.filter((d) => d === trimmedDomain).length > 1;
 
 		if (trimmedDomain === "") {
 			domainErrorMessage.textContent = "Domain is required";
@@ -196,76 +195,83 @@ function addDomainEmailPair(container, domain = "", email = "",saveButton) {
 }
 
 function validateEmail(email) {
-    const regex = /^[^\s@]+@gmail\.com$/;
-    return regex.test(email);
+	const regex = /^[^\s@]+@gmail\.com$/;
+	return regex.test(email);
 }
 
 async function handleSaveClick() {
+	const domainEmailContainers = document.querySelectorAll(
+		".domain-email-container"
+	);
 
-    const domainEmailContainers = document.querySelectorAll(".domain-email-container");
-	
-    const domainEmails = {};
+	const domainEmails = {};
 
-    let isValid = true;
+	let isValid = true;
 
-    domainEmailContainers.forEach(container => {
-        const domainInput = container.querySelector(".domain-input");
-        const emailInput = container.querySelector(".input-email-container input");
+	domainEmailContainers.forEach((container) => {
+		const domainInput = container.querySelector(".domain-input");
+		const emailInput = container.querySelector(
+			".input-email-container input"
+		);
 
-        const domain = domainInput.value.trim();
-        const email = emailInput.value.trim();
+		const domain = domainInput.value.trim();
+		const email = emailInput.value.trim();
 
 		if (domain === "" || email === "") isValid = false;
 
-        if (domainEmails[domain]) {
-            isValid = false;
-            container.querySelector(".error-message:first-of-type").style.display = "block";
-        } else {
-            domainEmails[domain] = email;
-        }
+		if (domainEmails[domain]) {
+			isValid = false;
+			container.querySelector(
+				".error-message:first-of-type"
+			).style.display = "block";
+		} else {
+			domainEmails[domain] = email;
+		}
 
-        if (!validateEmail(email)) {
-            isValid = false;
-            container.querySelector(".error-message:last-of-type").style.display = "block";
-        }
-    });
+		if (!validateEmail(email)) {
+			isValid = false;
+			container.querySelector(
+				".error-message:last-of-type"
+			).style.display = "block";
+		}
+	});
 
-    if (!isValid) {
-        return;
-    }
+	if (!isValid) {
+		return;
+	}
 
-    await setStorage("domainEmails", domainEmails);
+	await setStorage("domainEmails", domainEmails);
 
-    let tabs = await chrome.tabs.query({ active: true, currentWindow: true });
-    if (!tabs || (tabs && tabs.length == 0)) {
-        console.error("No active tab found");
-    }
-    await chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        function: () => {
-            window.location.reload();
-        },
-    });
-    window.close();
+	let tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+	if (!tabs || (tabs && tabs.length == 0)) {
+		console.error("No active tab found");
+	}
+	await chrome.scripting.executeScript({
+		target: { tabId: tabs[0].id },
+		function: () => {
+			window.location.reload();
+		},
+	});
+	window.close();
 }
 
 function validateAndMutateKey(key) {
-    if (typeof key !== "string") throw new Error("Invalid key");
-    return key;
+	if (typeof key !== "string") throw new Error("Invalid key");
+	return key;
 }
 
 async function getFromStorage(key) {
-    let storageKey = validateAndMutateKey(key);
-    return await chrome.storage.local.get([storageKey]);
+	let storageKey = validateAndMutateKey(key);
+	return await chrome.storage.local.get([storageKey]);
 }
 
 async function setStorage(key, value) {
-    let storageKey = validateAndMutateKey(key);
-    return await chrome.storage.local.set({ [storageKey]: value });
+	let storageKey = validateAndMutateKey(key);
+	return await chrome.storage.local.set({ [storageKey]: value });
 }
 
 function enableSaveButton() {
 	let button = document.getElementById("saveButton");
-    button.classList.remove("disabled")
-    button.disabled=false
+	button.classList.remove("disabled");
+	button.disabled = false;
 }
